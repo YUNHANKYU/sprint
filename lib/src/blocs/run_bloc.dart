9faa416e6e2624cf7deb61e3897f84e840f971f5
YUNHANKYU <bhgt4563@gmail.com>
2019-02-10 15:07:47 +0900

    add runBloc

--- lib/src/blocs/run_bloc.dart
+++ lib/src/blocs/run_bloc.dart
@@ -3,8 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginBloc extends Object {
+class RunBloc extends Object {
+  final _loggedIn = StreamController<bool>.broadcast();
 
+  Stream<bool> get loggedIn => _loggedIn.stream;
+
+  Function(bool) get setLoggedIn => _loggedIn.sink.add;
+
+  dispose() {
+    _loggedIn.close();
+  }
 
 
 }
